ef141358772a0b7f7cfe395f418eb25bb0ca869b
Klap.ir <mohammadrezaasiyaban2286@gmail.com>
2023-03-13 03:06:34 +0330

    init develop

---
+++ app/src/main/java/com/example/weatherapplication/fragment/DetailsFragment.java
@@ -0,0 +1,23 @@
+package com.example.weatherapplication.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.weatherapplication.R;
+import com.example.weatherapplication.base.BaseFragment;
+import com.example.weatherapplication.databinding.FragmentHomeBinding;
+
+public class DetailsFragment extends BaseFragment {
+    private FragmentHomeBinding binding;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding=FragmentHomeBinding.inflate(inflater,container,false);
+        return binding.getRoot();
+    }
+}
