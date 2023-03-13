e9d331ae97eaf620370dcd949ea42e47b133abf7
Klap.ir <mohammadrezaasiyaban2286@gmail.com>
2023-03-13 14:05:06 +0330

    add fragment

--- app/src/main/java/com/example/weatherapplication/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/weatherapplication/fragment/ProfileFragment.java
@@ -10,17 +10,14 @@ import android.view.ViewGroup;
 
 import com.example.weatherapplication.R;
 import com.example.weatherapplication.base.BaseFragment;
-import com.example.weatherapplication.databinding.FragmentHomeBinding;
+import com.example.weatherapplication.databinding.FragmentProfileBinding;
 
-public class DetailsFragment extends BaseFragment {
-    private FragmentHomeBinding binding;
+public class ProfileFragment extends BaseFragment {
+    private FragmentProfileBinding binding;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding=FragmentHomeBinding.inflate(inflater,container,false);
+        binding=FragmentProfileBinding.inflate(inflater,container,false);
         return binding.getRoot();
-
-
-
     }
 }
--- app/src/main/java/com/example/weatherapplication/fragment/SettingFragment.java
+++ app/src/main/java/com/example/weatherapplication/fragment/SettingFragment.java
@@ -1,26 +1,19 @@
 package com.example.weatherapplication.fragment;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.weatherapplication.R;
 import com.example.weatherapplication.base.BaseFragment;
-import com.example.weatherapplication.databinding.FragmentHomeBinding;
+import com.example.weatherapplication.databinding.FragmentSettingBinding;
 
-public class DetailsFragment extends BaseFragment {
-    private FragmentHomeBinding binding;
+public class SettingFragment extends BaseFragment {
+private FragmentSettingBinding binding;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding=FragmentHomeBinding.inflate(inflater,container,false);
+        binding=FragmentSettingBinding.inflate(inflater,container,false);
         return binding.getRoot();
-
-
-
     }
 }
